76f0164b167c8c4e7197c2aa7a02af00c2e394bd
Boss2021-eng <77675650+Boss2021-eng@users.noreply.github.com>
2022-06-24 20:51:45 +0100

    added comments

--- life.js
+++ life.js
@@ -1,13 +1,29 @@
+// Enter your arithmetic operation
 let  sign = prompt("Please enter your arithmetic operation")
 
+// Check for your arithmetic operation
 if (sign === "+"){
+
+    // Takes the user input of the first number and converts it to a number
     var firstNum = Number(prompt("Please enter the first number ")) 
+
+    //  Check if the user input is a number
     if (isNaN(firstNum)){
+
+        //  if not a number prompt the user to enter a number
         alert("Please enter a number")}
 
+        // Takes the user input of the second number and converts it to a number
     else{var secondNum = Number(prompt("Please enter the second number "))
+
+        //  Check if the user input is a number
+        
         if(isNaN(secondNum)){
+
+            //  if not prompt the user to enter a number
             alert("Please enter a number")}
+            
+            //  if not prompt the user to enter a number
             else{alert(firstNum + secondNum)}  
         }
         
