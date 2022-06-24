32c56785acaabb88fe2c280b211dabb5a5ca194c
Boss2021-eng <77675650+Boss2021-eng@users.noreply.github.com>
2022-06-24 14:55:38 +0100

    initial commit

---
+++ live.js
@@ -0,0 +1,10 @@
+let my_name = "Damilola Olatayo"
+let my_height = "1.75m"
+let my_country = "Nigeria"
+
+document.write("My name is: " + my_name)
+document.write("<br>")
+document.write("My height is: " + my_height)
+document.write("<br>")
+document.write("My country is: " + my_country)
+
